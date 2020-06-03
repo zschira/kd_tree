3392187c90f8301edfb7ef6a47c2f80704f452fa
zschira <zachary.w.schira@nasa.gov>
2020-06-03 12:11:34 -0400

    Add trait implementation and test for vec<f32>

--- src/lib.rs
+++ src/lib.rs
@@ -27,12 +27,37 @@ impl Point<f64> for Vec<f64> {
     }
 }
 
+impl Point<f32> for Vec<f32> {
+    fn distance(&self, other: &Self) -> Result<f32, KdError> {
+        if self.len() != other.len() {
+            return Err(KdError::DimensionError);
+        }
+
+        let mut distance = 0f32;
+        for i in 0..self.len() {
+            let diff = self[i] - other[i];
+            distance += diff * diff; 
+        }
+        Ok(distance.sqrt())
+    }
+
+    fn greater(&self, other: &Self, cur_dimension: usize) -> bool {
+        self[cur_dimension] > other[cur_dimension]
+    }
+
+    fn split_plane(&self, cur_dimension: usize) -> Vec<f32> {
+        let mut plane = vec![0f32; self.len()];
+        plane[cur_dimension] = self[cur_dimension];
+        plane
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::kd_tree::KdTree;
     use std::time::{Instant};
     #[test]
-    fn it_works() {
+    fn test_vecf64() {
         let mut tree = KdTree::<Vec<f64>, f64>::with_capacity(3, 1_000_000);
 
         println!("Constructing tree...");
@@ -50,6 +75,52 @@ mod tests {
 
         for _i in 1..2 {
             let query_point = vec![rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>()];
+
+            println!("Brute force");
+            let now = Instant::now();
+            let brute_result = tree.brute_force(&query_point);
+            println!("Brute force finished in {}us", now.elapsed().as_micros());
+
+            println!("KD-Search");
+            let now = Instant::now();
+            let search_result = tree.find_closest(&query_point);
+            println!("KD-Search finished in {}us", now.elapsed().as_micros());
+            
+            assert_eq!(search_result.is_ok(), true);
+            match search_result {
+                Ok((point, distance)) => {
+                    if let Ok((point_brute, dist_brute)) = brute_result {
+                        println!("KD: {}, BRUTE: {}", distance, dist_brute);
+                        assert!(distance == dist_brute);
+                        assert!(point[0] == point_brute[0]);
+                        assert!(point[1] == point_brute[1]);
+                        assert!(point[2] == point_brute[2]);
+                    }
+                },
+                Err(e) => { println!("{}", e); }
+            }
+        }
+    }
+
+    #[test]
+    fn test_vecf32() {
+        let mut tree = KdTree::<Vec<f32>, f32>::with_capacity(3, 1_000_000);
+
+        println!("Constructing tree...");
+        let now = Instant::now();
+        for _i in 1..1_000_000 {
+            match tree.add_point(vec![rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()]) {
+                Ok(()) => { },
+                Err(e) => {
+                    println!("Failed to add point: {}", e);
+                    assert!(false);
+                },
+            }
+        }
+        println!("Tree generated generated in {}us", now.elapsed().as_micros());
+
+        for _i in 1..2 {
+            let query_point = vec![rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()];
 
             println!("Brute force");
             let now = Instant::now();
